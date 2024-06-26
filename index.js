f0f6505a531fb7c15c01290ad5c5903162a91340
wdk <fredddray@gmail.com>
2024-06-26 18:11:14 +0800

    26.06 commit

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
-let calculate = [];
+const calculate = [];
+
+const obj = {
+  pageName: "",
+  answer: "",
+};
 
 document.querySelectorAll(".variant").forEach((variant) => {
   variant.addEventListener("click", () => {
+    const ans = variant.querySelector(".variant__description").innerHTML.trim();
+
+    obj.pageName = document
+      .getElementsByClassName("calculate__test-title")
+      [currentIndex].innerHTML.trim();
+    obj.answer = ans;
+
     document.querySelectorAll(".variant").forEach((sq) => {
       sq.classList.remove("selected");
-      calculate = [];
+
+      obj.answer = ans;
     });
-    const ans = variant.querySelector(".variant__description").innerHTML.trim();
+
     variant.classList.add("selected");
-    calculate.push(ans);
   });
 });
 
@@ -19,26 +31,30 @@ document.addEventListener("DOMContentLoaded", function () {
     .querySelector(".calculate")
     .querySelectorAll(".container");
   const buttons = document.querySelectorAll(".calculate__next");
-  let currentIndex = 0;
 
   buttons.forEach((button) => {
     button.addEventListener("click", function () {
-      console.log("asd");
+      console.log(obj);
+      calculate.push(obj);
+      console.log(calculate);
+
+      obj.pageName = "";
+      obj.answer = "";
+
       if (currentIndex < containers.length) {
-        // Fade out current container
         containers[currentIndex].classList.remove("show");
+
         setTimeout(() => {
           containers[currentIndex].classList.add("dnone");
 
-          // Update index for next container
-          currentIndex = (currentIndex + 1) % containers.length;
+          currentIndex += 1;
 
-          // Fade in next container
+          if (currentIndex == containers.length - 1) startTimer();
           containers[currentIndex].classList.remove("dnone");
           setTimeout(() => {
             containers[currentIndex].classList.add("show");
           }, 10);
-        }, 500); // Wait for fade out transition to complete
+        }, 500);
       }
     });
   });
@@ -70,3 +86,22 @@ formInputs.forEach((input) => {
     mask: "+{7} (000) 000 00-00",
   });
 });
+
+$("#daySlider").slick();
+
+const startTimer = () => {
+  var start = Date.now();
+  const timer = 360;
+  setInterval(function () {
+    var delta = Date.now() - start; // milliseconds elapsed since start
+    const currentTime = timer - Math.floor(delta / 1000); // in seconds
+    const minutes = Math.floor(currentTime / 60);
+    const seconds = currentTime % 60;
+
+    if (seconds >= 10) var str = `0${minutes} : ${seconds}`;
+    else var str = `0${minutes} : 0${seconds}`;
+
+    const element = document.getElementById("timerTime");
+    element.innerHTML = str;
+  }, 1000);
+};
